443843bcae603dc10dde1ff53fd8c2c893bdfe56
ach <ignaciomenchaca@gmail.com>
2018-05-26 20:22:07 -0300

    Clase 5

--- catch-of-the-day/src/components/SotrePicker.js
+++ catch-of-the-day/src/components/SotrePicker.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 // cada componente creado sera una clase 
 class StorePicker extends React.Component {
@@ -6,7 +6,29 @@ class StorePicker extends React.Component {
   // ese metodo se llama "render()"
   // se encarga de volvar la estructura html 
   render() {
-    return <p>Adentro de storepicker</p>
+    return ( // los parentesis son necesarios
+      // cuando se quiere escribir html en varias lineas
+
+      // solo se puede retornar un elemento html.
+      // esto no es posible:
+      // <h2></h2>
+      // <form action=""></form>
+
+      // Una forma de solucionarlo es meter toda la
+      // estructura dentro de:
+      // <React.Fragment>
+      // </React.Fragment>
+
+      <Fragment>
+        {/* <p>Fish!</p> */}
+        <form className="store-selector">
+          { /* comentario dentro de JSX */ }
+          <h2>Please Enter a Store</h2>
+          <input type="text" required="required" placeholder="Store Name"/>
+          <button type="submit">Visit Store</button>
+        </form>      
+      </Fragment>
+    )
     // para que esto se muestre, primero hay que montar
     // la aplicacion con el paquete 'react-DOM'
   }
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,15 @@
-// let's go!
+import React from 'react';
+// pordria importarse solo la clase componente
+// en lugar de toda la libreria:
+// import { Component } from 'react'
+// en este caso, al extender la clase seria:
+// class StorePicker extends Component {}
+
+import { render } from 'react-dom';
+// en este caso solo importa el metodo "render" del paquete
+// react-dom
+
+import StorePicker from '../src/components/SotrePicker';
+
+// render(expresion JSX (html), elemento dom para montar)
+render(<StorePicker />, document.querySelector('#main'));
