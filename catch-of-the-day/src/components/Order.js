57c2a7e6a4c7658fcfd8ab44ebabc99df06dc366
ach <ignaciomenchaca@gmail.com>
2018-05-26 20:44:04 -0300

    Clase 7

---
+++ catch-of-the-day/src/components/Order.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Order extends React.Component {
+  render() {
+    return (
+      <div className="order">Componente Order</div>
+    )
+  }
+}
+
+export default Order;
