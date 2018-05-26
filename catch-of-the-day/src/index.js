39fb6089efcf0f6afc0730a77db3668fd6d3cdb6
ach <ignaciomenchaca@gmail.com>
2018-05-26 20:26:31 -0300

    Clase 6

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -11,5 +11,9 @@ import { render } from 'react-dom';
 
 import StorePicker from '../src/components/SotrePicker';
 
+// importar los estilos para que esten disponibles en toda
+// la aplicacion
+import './css/style.css';
+
 // render(expresion JSX (html), elemento dom para montar)
 render(<StorePicker />, document.querySelector('#main'));
