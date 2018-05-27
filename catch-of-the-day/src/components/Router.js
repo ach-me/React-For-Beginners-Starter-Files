1b8ccf0ef9306182dba83b53d4d5044f6256e016
ach <ignaciomenchaca@gmail.com>
2018-05-26 21:30:55 -0300

    Clase 10

---
+++ catch-of-the-day/src/components/Router.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import StorePicker from './SotrePicker';
+import App from './App';
+import NotFound from './NotFound';
+
+// stateless functional component
+// (cuando el componente solo retorna html)
+const Router = () => (
+  <BrowserRouter>
+    <Switch>
+      {/* Aqui van todos los ruteos. Las rutas se van intentando secuencialmente de arriba hacia abajo */}
+      <Route exact path="/" component={StorePicker}/>
+      <Route path="/store/:storeId" component={App}/>
+      <Route component={NotFound}/>
+    </Switch>
+  </BrowserRouter>
+)
+
+export default Router;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -9,12 +9,13 @@ import { render } from 'react-dom';
 // en este caso solo importa el metodo "render" del paquete
 // react-dom
 
-import StorePicker from '../src/components/SotrePicker';
-import App from '../src/components/App';
+// import StorePicker from '../src/components/SotrePicker';
+// import App from '../src/components/App';
+import Router from '../src/components/Router';
 
 // importar los estilos para que esten disponibles en toda
 // la aplicacion
 import './css/style.css';
 
 // render(expresion JSX (html), elemento dom para montar)
-render(<App />, document.querySelector('#main'));
+render(<Router />, document.querySelector('#main'));
